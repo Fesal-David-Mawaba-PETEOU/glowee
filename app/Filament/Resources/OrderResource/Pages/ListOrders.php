31cbbe394c98a753f25b61592db2c0d0f59463c7
Fesal David Mawaba PETEOU <peteoudavid13@gmail.com>
2025-04-17 11:29:04 +0000

    Config Orders stats widget and Tab group

--- app/Filament/Resources/OrderResource/Pages/ListOrders.php
+++ app/Filament/Resources/OrderResource/Pages/ListOrders.php
@@ -3,8 +3,11 @@
 namespace App\Filament\Resources\OrderResource\Pages;
 
 use App\Filament\Resources\OrderResource;
+use App\Filament\Resources\OrderResource\Widgets\OrderStats;
+use App\Models\Order;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
+use Filament\Resources\Components\Tab;
 
 class ListOrders extends ListRecords
 {
@@ -16,4 +19,34 @@ class ListOrders extends ListRecords
             Actions\CreateAction::make(),
         ];
     }
+
+    protected function getHeaderWidgets(): array
+    {
+        return [
+            OrderStats::class,
+        ];
+    }
+
+    public function getTabs(): array
+    {
+        return [
+            null => Tab::make('All Orders'),
+            'new' => Tab::make()->query(fn ($query) => $query->where('status', 'new'))
+                ->label('New Orders')
+                ->icon('heroicon-o-shopping-cart'),
+            'processing' => Tab::make()->query(fn ($query) => $query->where('status', 'processing'))
+                ->label('Processing Orders')
+                ->icon('heroicon-o-cog'),
+            'shipped' => Tab::make()->query(fn ($query) => $query->where('status', 'shipped'))
+                ->label('Shipped Orders')
+                ->icon('heroicon-o-truck'),
+            'delivered' => Tab::make()->query(fn ($query) => $query->where('status', 'delivered'))
+                ->label('Delivered Orders')
+                ->icon('heroicon-o-check-circle'),
+            'cancelled' => Tab::make()->query(fn ($query) => $query->where('status', 'cancelled'))
+                ->label('Cancelled Orders')
+                ->icon('heroicon-o-x-circle'),
+        ];
+    }
+
 }
---
+++ app/Filament/Resources/OrderResource/Widgets/OrderStats.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Filament\Resources\OrderResource\Widgets;
+
+use Filament\Widgets\StatsOverviewWidget as BaseWidget;
+use Filament\Widgets\StatsOverviewWidget\Stat;
+use App\Models\Order;
+
+class OrderStats extends BaseWidget
+{
+    protected function getStats(): array
+    {
+        return [
+            Stat::make('New Orders', Order::query()->where('status', 'new')->count())
+                ->color('success')
+                ->icon('heroicon-o-shopping-cart'),
+
+            Stat::make('Shipped Orders', Order::query()->where('status', 'shipped')->count())
+                ->color('warning')
+                ->icon('heroicon-o-truck'),
+            
+            Stat::make('Processing Orders', Order::query()->where('status', 'processing')->count())
+                ->color('warning')
+                ->icon('heroicon-o-cog'),
+
+            Stat::make('Completed Orders', Order::query()->where('status', 'delivered')->count())
+                ->color('primary')
+                ->icon('heroicon-o-check-circle'),
+
+            Stat::make('Cancelled Orders', Order::query()->where('status', 'cancelled')->count())
+                ->color('danger')
+                ->icon('heroicon-o-x-circle'),
+
+            Stat::make('Average Price', 'XOF ' . number_format(Order::query()->avg('grand_total'), 2))
+                ->color('primary'),
+        ];
+    }
+}
