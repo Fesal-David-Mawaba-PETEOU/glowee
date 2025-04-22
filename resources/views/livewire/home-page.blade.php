855ae0974e2c92d84c803108719252c8e51bd8a1
jiren1301 <peteoudavid13@gmail.com>
2025-04-22 00:26:02 +0000

    Customer Panel (install and config Tailwindcss, livewire3, preline etc.)

---
+++ resources/views/livewire/home-page.blade.php
@@ -0,0 +1,3 @@
+<div>
+    <h1 class="text-2xl text-blue-500">Hello laravel, it's me !</h1>
+</div>
--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,18 @@
-import {
-    defineConfig
-} from 'vite';
+import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: [`resources/views/**/*`],
+            refresh: true,
         }),
-        tailwindcss(),
     ],
     server: {
-        cors: true,
+        host: '127.0.0.1',
+        port: 5173,
+        hmr: {
+            host: '127.0.0.1'
+        }
     },
 });
